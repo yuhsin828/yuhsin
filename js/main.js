9ade7df4f531a8f2b6b8639b7867c08dbfd525d1
yuhsin828 <71396690+yuhsin828@users.noreply.github.com>
2023-11-02 14:44:08 +0800

    goTop button & footer

--- js/main.js
+++ js/main.js
@@ -15,6 +15,18 @@ $('#scrollDown').click(function () {
     $('html,body').animate({ scrollTop: topMain }, 200);
 });
 
+/* 回到頂部 */
+$('#goTop').click(function () {
+    $('html,body').animate({ scrollTop: 0 }, 200);
+});
+$(window).scroll(function () {
+    if ($(this).scrollTop() > main) {
+        $('#goTop').fadeIn(200);
+    } else {
+        $('#goTop').stop().fadeOut(200);
+    }
+}).scroll();
+
 /* 啟動wow */
 $(function () {
     new WOW().init();
