6f026fadd5862327b427211d106976ca0b159956
yuhsin828 <71396690+yuhsin828@users.noreply.github.com>
2023-11-03 11:33:09 +0800

    modify sitemap

---
+++ js/home.js
@@ -0,0 +1,22 @@
+// const main = $('#scrollDown').offset().top;
+
+/* nav固定在上方 */
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= main) {  /* 當滑動到scrollDown */
+        $('.navbar').removeClass('d-none');
+    } else {
+        $('.navbar').addClass('d-none');
+    }
+});
+
+/* 往下滑 */
+$('#scrollDown').click(function () {
+    const topMain = $('#section1').offset().top - $('.navbar').innerHeight();
+    $('html,body').animate({ scrollTop: topMain }, 200);
+});
+
+
+/* 啟動wow */
+$(function () {
+    new WOW().init();
+})
--- js/main.js
+++ js/main.js
@@ -1,20 +1,5 @@
 const main = $('#scrollDown').offset().top;
 
-/* nav固定在上方 */
-$(window).scroll(function () {
-    if ($(this).scrollTop() >= main) {  /* 當滑動到scrollDown */
-        $('.navbar').removeClass('d-none');
-    } else {
-        $('.navbar').addClass('d-none');
-    }
-});
-
-/* 往下滑 */
-$('#scrollDown').click(function () {
-    const topMain = $('#section1').offset().top - $('.navbar').innerHeight();
-    $('html,body').animate({ scrollTop: topMain }, 200);
-});
-
 /* 回到頂部 */
 $('#goTop').click(function () {
     $('html,body').animate({ scrollTop: 0 }, 200);
@@ -26,8 +11,3 @@ $(window).scroll(function () {
         $('#goTop').stop().fadeOut(200);
     }
 }).scroll();
-
-/* 啟動wow */
-$(function () {
-    new WOW().init();
-})
